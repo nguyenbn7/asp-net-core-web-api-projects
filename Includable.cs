4c5de32e93ce23022568cada6a9d5bfac01d0a9e
nguyenbn7 <148407637+nguyenbn7@users.noreply.github.com>
2025-03-17 15:51:59 +0700

    init includable

---
+++ Includable.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+
+namespace GenericRepository;
+
+public class Includable<TEntity> where TEntity : class
+{
+    public List<Expression<Func<TEntity, dynamic>>> Expressions { get; private set; } = [];
+
+    public Includable<TEntity> Include(Expression<Func<TEntity, dynamic>> expression)
+    {
+        Expressions.Add(expression);
+        return this;
+    }
+}
